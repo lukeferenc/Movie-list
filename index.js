b0b78830e5513dc89f6fb6f9f7b0adcc750c1114
lukeferenc <luke.theduke.of.cornwall@gmail.com>
2021-07-22 21:24:05 +0200

    2.10 onlinedatabase

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ const Directors = Models.Director;
 const { check, validationResult } = require('express-validator');
 
 
-mongoose.connect('mongodb://localhost:27017/test', { 
-	useNewUrlParser: true, 
-	useUnifiedTopology: true 
-});
+// mongoose.connect('mongodb://localhost:27017/test', { 
+//	useNewUrlParser: true, 
+//	useUnifiedTopology: true 
+// });
+
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 
